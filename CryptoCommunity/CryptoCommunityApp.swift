87fc1d314b84a03ce2224f8b8060c5e58aa03673
Vy4chesl4vOS <vy4chesl4v.u@icloud.com>
2022-04-09 17:11:20 +0300

    Initial Commit

---
+++ CryptoCommunity/CryptoCommunityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCommunityApp.swift
+//  CryptoCommunity
+//
+//  Created by Вячеслав Утешев on 09.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCommunityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
